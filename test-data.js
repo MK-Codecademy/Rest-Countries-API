ec7c429d64d2152ac2be0d91244431bfeaf40870
Will Garrett <willgarrett64@yahoo.com>
2021-07-27 22:25:21 +0100

    create test data object to match countries shown in design home page

---
+++ test-data.js
@@ -0,0 +1,50 @@
+const testCountries = [
+  {
+    country: "Germany",
+    population: 81770900,
+    region: "Europe",
+    capital: "Berlin"
+  },
+  {
+    country: "United States of America",
+    population: 323947000,
+    region: "Americas",
+    capital: "Washington, D.C."
+  },
+  {
+    country: "Brazil",
+    population: 206135893,
+    region: "Americas",
+    capital: "Brasília"
+  },
+  {
+    country: "Iceland",
+    population: 334300,
+    region: "Europe",
+    capital: "Reykjavik"
+  },
+  {
+    country: "Afghanistan",
+    population: 27657145,
+    region: "Asia",
+    capital: "Kabul"
+  },
+  {
+    country: "Aland Islands",
+    population: 28875,
+    region: "Europe",
+    capital: "Mariehamn"
+  },
+  {
+    country: "Albania",
+    population: 2886026,
+    region: "Europe",
+    capital: "Tirana"
+  },
+  {
+    country: "Algeria",
+    population: 40400000,
+    region: "Africa",
+    capital: "Algiers"
+  },
+];
