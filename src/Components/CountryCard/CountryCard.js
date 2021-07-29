93a85431e6a637f5e8a1e6a668c5acb1aaa71b47
piercy87 <72551589+piercy87@users.noreply.github.com>
2021-07-29 19:15:59 +0100

    Styles on cards

--- src/Components/CountryCard/CountryCard.js
+++ src/Components/CountryCard/CountryCard.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import styles from "./CountryCard.module.css"
+import React from "react";
+import Card from "@material-ui/core/Card";
+import styles from "./CountryCard.module.css";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
-export default function CountryCard({country})   {
-    return  (
-
-        <Card className={styles.card}>
-            <h2 className='countryTitle'>{country.country}</h2>
-            <p>{`Population: - ${country.population}`}</p>
-            <p>{`Capital City ${country.capital}`}</p>
-            <p>{`Region - ${country.region}`}</p>
-        </Card>
-    )
+export default function CountryCard({ country }) {
+  return (
+    <Card className={styles.card}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {country.country}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {`Population: - ${country.population}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {`Capital City - ${country.capital}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {`Region - ${country.region}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
