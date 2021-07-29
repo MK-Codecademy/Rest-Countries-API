e24008e70dc3ce7b41de76361cc4c5fe4869f088
piercy87 <72551589+piercy87@users.noreply.github.com>
2021-07-29 18:44:17 +0100

    Create module css

--- src/Components/CountryCardList/CountryCardList.js
+++ src/Components/CountryCardList/CountryCardList.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./CountryCardList.css";
+import styles from "./CountryCardList.module.css";
 import Card from "@material-ui/core/Card";
 import Paper from '@material-ui/core/Paper';
-
 import CountryCard from "../CountryCard/CountryCard";
+
 
 // import test data - will be replaced when API call is functional
 import { testCountries } from "../../test-data";
@@ -11,8 +11,8 @@ import { testCountries } from "../../test-data";
 export default function CountryCardList(props) {
   return (
     <div className="wrapper ">
-      <Paper elevation={3} className='countyCardWarpper'>
-        <p>I would like to contain the list of all the country cards</p>
+      <Paper elevation={3} className={styles.countyCardWarpper}>
+        <h1>{testCountries.length > 0 ? 'Countries' : ''}</h1>
         {testCountries.map((country) => (
           <CountryCard country={country} />
         ))}
