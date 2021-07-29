010bc534607e97aeb5344d79bc8cd270d31ca8a9
piercy87 <72551589+piercy87@users.noreply.github.com>
2021-07-29 18:26:05 +0100

    Changes to cards and wrappers

--- src/Components/CountryCard/CountryCard.js
+++ src/Components/CountryCard/CountryCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './CountryCard.css';
+import Card from '@material-ui/core/Card';
 
-export default function CountryCard(props)   {
+export default function CountryCard({country})   {
     return  (
-        <div className="card border">
-            Hi I want to be a CountryCard component please
-        </div>
+
+        <Card className="card border">
+            <h2 className='countryTitle'>{country.country}</h2>
+            <p>{`Population: - ${country.population}`}</p>
+            <p>{`Capital City ${country.capital}`}</p>
+            <p>{`Region - ${country.region}`}</p>
+        </Card>
     )
 }
--- src/Components/CountryCardList/CountryCardList.js
+++ src/Components/CountryCardList/CountryCardList.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import './CountryCardList.css';
+import React from "react";
+import "./CountryCardList.css";
+import Card from "@material-ui/core/Card";
+import Paper from '@material-ui/core/Paper';
 
-import CountryCard from '../CountryCard/CountryCard';
+import CountryCard from "../CountryCard/CountryCard";
 
 // import test data - will be replaced when API call is functional
-import { testCountries } from '../../test-data';
+import { testCountries } from "../../test-data";
 
-export default function CountryCardList(props)   {
-    return(
-      <div className="wrapper border">
+export default function CountryCardList(props) {
+  return (
+    <div className="wrapper ">
+      <Paper elevation={3} className='countyCardWarpper'>
         <p>I would like to contain the list of all the country cards</p>
-        {testCountries.map(country => <CountryCard />)}
-      </div>
-    )
-    }
+        {testCountries.map((country) => (
+          <CountryCard country={country} />
+        ))}
+      </Paper>
+    </div>
+  );
+}
