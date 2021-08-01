e3816dce4c5b6c58f4136ec3576c9bb35659a392
Will Garrett <willgarrett64@yahoo.com>
2021-08-01 22:12:11 +0100

    fix bug with clicking on p element in filter list

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -18,7 +18,7 @@ export default function Filter(props) {
   // toggle if region filter is selected
   const handleSelect = (e) => {
     const selectedCopy = { ...selected };
-    const region = e.target.id;
+    const region = e.target.tagName === "P" ? e.target.parentNode.id : e.target.id;
     selectedCopy[region] = !selected[region];
     setSelected(selectedCopy)
     dispatch(updateFilter(e.target.id))
@@ -32,27 +32,27 @@ export default function Filter(props) {
       </div>
       <ul className={`dropList dmElement corners ${open && 'open'}`} id="dropList">
         <li className="dropItem" id="africa" key="africa" onClick={handleSelect}>
-          <p id="africa" >Africa</p>
+          <p onClick={handleSelect} >Africa</p>
           {selected.africa ? <BiCheck /> : null}
         </li>
         <li className="dropItem" id="americas" key="americas" onClick={handleSelect}>
-          <p id="americas" >Americas</p>
+          <p onClick={handleSelect} >Americas</p>
           {selected.americas ? <BiCheck /> : null}
         </li>
         <li className="dropItem" id="asia" key="asia"onClick={handleSelect} >
-          <p id="asia" >Asia</p>
+          <p onClick={handleSelect} >Asia</p>
           {selected.asia ? <BiCheck /> : null}
         </li>
         <li className="dropItem" id="europe" key="europe" onClick={handleSelect} >
-          <p id="europe" >Europe</p>
+          <p onClick={handleSelect} >Europe</p>
           {selected.europe ? <BiCheck /> : null}
         </li>
         <li className="dropItem" id="oceania" key="oceania" onClick={handleSelect} >
-          <p id="oceania" >Oceania</p>
+          <p onClick={handleSelect} >Oceania</p>
           {selected.oceania ? <BiCheck /> : null}
         </li>
         <li className="dropItem" id="polar" key="polar" onClick={handleSelect} >
-          <p id="polar" >Polar</p>
+          <p onClick={handleSelect} >Polar</p>
           {selected.polar ? <BiCheck /> : null}
         </li>
       </ul>
