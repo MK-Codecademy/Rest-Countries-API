0553dc9a7814243705d626b4a348c89574ff3bfa
piercy87 <72551589+piercy87@users.noreply.github.com>
2021-08-01 12:55:40 +0100

    Update darkModeSlice.js

--- src/features/darkModeSlice.js
+++ src/features/darkModeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const darkModeSlice = createSlice({
     name: 'darkMode',
     initialState: {
-      value: false
+      value: true
     },
     reducers: {
       toggleDarkMode: (state) => {
