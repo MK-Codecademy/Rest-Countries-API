66a602fc41914f62645b9a5c3fde6961d2ddb492
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-01 09:35:07 +0100

    styled SearchFilterWrapper

--- src/Components/SearchFilterWrapper/SearchFilterWrapper.js
+++ src/Components/SearchFilterWrapper/SearchFilterWrapper.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import './SearchFilterWrapper.css'
+import React from "react";
+import "./SearchFilterWrapper.css";
+import Filter from "../Filter/Filter";
+import SearchBar from "../Searchbar/SearchBar";
 
-import Filter from '../Filter/Filter';
-import SearchBar from '../Searchbar/SearchBar';
-
-export default function Search_Filter_wrapper(props)    {
-    return(
-        <div className="wrapper border">
-            <p>I would like to wrap the Search and Filter please</p>
-            <SearchBar />
-            <Filter />
-        </div>
-    )
+export default function Search_Filter_wrapper(props) {
+  return (
+    <div className="wrapper">
+      <SearchBar className="searchBar" />
+      <Filter />
+    </div>
+  );
 }
