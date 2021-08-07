56a6eae2c17a33a570012cfb12982e9fb9b978bb
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-07 09:52:21 +0100

    added back button

---
+++ src/Components/BackButton/BackButton.js
@@ -0,0 +1,22 @@
+import styles from "./BackButton.css";
+import { BsArrowLeft } from "react-icons/bs";
+import { useHistory } from "react-router-dom";
+
+function BackButton() {
+  const history = useHistory();
+
+  const handleRoute = () => {
+    history.push("/");
+  };
+
+  return (
+    <div className="container">
+      <button className="button" onClick={handleRoute}>
+        <BsArrowLeft className="btnArrow" />
+        Back
+      </button>
+    </div>
+  );
+}
+
+export default BackButton;
