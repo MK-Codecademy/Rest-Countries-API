3ab12aac50c2cde14270e542b70bfab1299079e8
Will Garrett <willgarrett64@yahoo.com>
2021-08-09 14:42:03 +0100

    display all country data required in CountryInfoPage

---
+++ src/Components/CountryInfo/CountryInfo.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "./CountryInfo.css";
+
+
+function CountryInfo({countryData}) {
+  const currencyArray = [];
+  countryData.currencies.forEach(el => currencyArray.push(el.name + ' (' + el.symbol + ')'));
+
+  const languageArray = [];
+  countryData.languages.forEach(el => languageArray.push(el.name))
+
+  return (
+    <div className="countryInfo">
+      <h2>{countryData.name}</h2>
+      <div>
+        <p><strong>Native Name</strong>: {countryData.nativeName}</p>
+        <p><strong>Population</strong>: {countryData.population.toLocaleString()}</p>
+        <p><strong>Region</strong>: {countryData.region}</p>
+        <p><strong>Sub Region</strong>: {countryData.subregion}</p>
+        <p><strong>Capital</strong>: {countryData.capital}</p>
+      </div>
+      <div>
+        <p><strong>Top Level Domain</strong>: {countryData.topLevelDomain}</p>
+        <p><strong>Currencies</strong>: {currencyArray.join(', ')}</p>
+        <p><strong>Languages</strong>: {languageArray.join(', ')}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CountryInfo;
