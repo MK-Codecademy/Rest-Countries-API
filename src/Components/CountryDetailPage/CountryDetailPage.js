0c67e9291f6550d077daa2b8b24daac6a36523ef
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-09 22:00:46 +0100

    created flag component

--- src/Components/CountryDetailPage/CountryDetailPage.js
+++ src/Components/CountryDetailPage/CountryDetailPage.js
@@ -8,35 +8,39 @@ import { fetchData } from "../../requests/fetchData";
 import BackButton from "../BackButton/BackButton";
 import CountryInfo from "../CountryInfo/CountryInfo";
 import BorderTagList from "../BorderTags/BorderTagList";
-
+import Flags from "../Flags/Flags";
 
 function CountryDetailPage() {
   const [countryData, setCountryData] = useState();
   const { country } = useParams(); // this is the alpha2Code property from the country data
   const [borders, setBorders] = useState();
 
-
   useEffect(() => {
-    fetchData(`https://restcountries.eu/rest/v2/alpha/${country}`).then((res) => {
-      setCountryData(res);
+    fetchData(`https://restcountries.eu/rest/v2/alpha/${country}`).then(
+      (res) => {
+        setCountryData(res);
 
-      fetchData(`https://restcountries.eu/rest/v2/alpha?codes=${res.borders.join(';')}`).then((res) => {
-        setBorders(res);
-      })
-    });
+        fetchData(
+          `https://restcountries.eu/rest/v2/alpha?codes=${res.borders.join(
+            ";"
+          )}`
+        ).then((res) => {
+          setBorders(res);
+        });
+      }
+    );
   }, []);
-
-
   // as countryData is aquired async, we have to wait until it has finished fetching before we try to render the details. This is why we only render the <p> tag if countryData exists
   return (
     <div className="container">
       <BackButton />
-      {borders && 
-        <div> 
+      {borders && (
+        <div>
+          <Flags countryData={countryData} />
           <CountryInfo countryData={countryData} />
           <BorderTagList borders={borders} />
         </div>
-      }
+      )}
     </div>
   );
 }
